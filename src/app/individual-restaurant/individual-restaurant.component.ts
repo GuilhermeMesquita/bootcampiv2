6574a121ef0d8a9a8c97b27251a2ed1dc162e5a4
Guilherme Rocha <guilhermemesquitarch@gmail.com>
2021-07-28 15:10:16 -0300

    wip visibility individual restaurant dialog

--- src/app/individual-restaurant/individual-restaurant.component.ts
+++ src/app/individual-restaurant/individual-restaurant.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-individual-restaurant',
@@ -7,9 +10,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndividualRestaurantComponent implements OnInit {
 
-  constructor() { }
+  media_geral: Array<any> = [];
+  array_avaliacao: Array<number> = [];
+  avaliacao: number = 5;
+  estrelas_contador: number = 5;
+  avaliacao_usuario: number = 3;
+  array_avaliacao_usuario: Array<number> = [];
+  array_comentarios_usuarios_avaliacao: Array<number> = [];
+  comentario_usuario: any;
+  array_comentarios_usuarios: Array<any> = [
+    {
+      comentario: 'Realmente ótimo restaurante',
+      estrelas: 1,
+      comentadoEm: new Date()
+    }
+  ];
+
+  constructor(public dialogRef: MatDialogRef<IndividualRestaurantComponent>,
+    @Inject(MAT_DIALOG_DATA) public restaurante: any,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+    for (let index = 0; index < this.estrelas_contador; index++) {
+      this.array_avaliacao.push(index);
+    }
+    for (let index = 0; index < this.estrelas_contador; index++) {
+      this.array_avaliacao_usuario.push(index);
+    }
+    for (let index = 0; index < this.estrelas_contador; index++) {
+      this.array_comentarios_usuarios_avaliacao.push(index);
+    }
+    this.listComments(this.restaurante.estrelas)
+  }
+
+  showIcon(contagem: number, index: number) {
+    if (contagem >= index + 1) {
+      return 'star';
+    } else {
+      return 'star_border';
+    }
+  }
+
+  onClick(avaliacao: number) {
+    this.avaliacao_usuario = avaliacao;
+    return false;
+  }
+
+  sendComment() {
+    const comentario = {
+      comentario: this.comentario_usuario,
+      estrelas: this.avaliacao_usuario,
+      comentadoEm: new Date()
+    }
+
+    this.array_comentarios_usuarios.push(comentario)
+    this.listComments(this.restaurante.estrelas);
+  }
+
+  listComments(param: number) {
+    this.media_geral = [];
+    this.array_comentarios_usuarios.forEach((comentario: any) => {
+      this.media_geral.push(comentario.estrelas);
+    });
+    this.media_geral.push(param);
+    const sum = this.media_geral.reduce((a: number, b: number) => a + b, 0);
+    const toDisplay = Math.round(sum / this.media_geral.length);
+    return this.avaliacao = toDisplay;
   }
 
 }
