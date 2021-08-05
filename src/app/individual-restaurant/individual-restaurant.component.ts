845adfaad92240335dee75a9121c944b53a33051
Guilherme Rocha <guilhermemesquitarch@gmail.com>
2021-08-05 20:32:26 -0300

    almost finished fellasss

--- src/app/individual-restaurant/individual-restaurant.component.ts
+++ src/app/individual-restaurant/individual-restaurant.component.ts
@@ -42,10 +42,12 @@ export class IndividualRestaurantComponent implements OnInit {
     for (let index = 0; index < this.estrelas_contador; index++) {
       this.array_comentarios_usuarios_avaliacao.push(index);
     }
-    this.listComments(this.restaurante.estrelas);
+
     this._auth_service.user$.subscribe(userInfos => {
       this.usuario_logado = userInfos;
     });
+
+    this.listComments(this.restaurante.estrelas);
   }
 
   showIcon(contagem: number, index: number) {
@@ -76,20 +78,16 @@ export class IndividualRestaurantComponent implements OnInit {
 
   listComments(param: number) {
     this.media_geral = [];
-    this._restaurants_service.listRestaurantComment(this.restaurante.id)
-      .subscribe(comentarios => {
-        this.comentario_usuario = comentarios.map(comentario => comentario);
-        comentarios.forEach(comentario => {
-          return this.media_geral.push(comentario["estrelas"]);
-        });
+    this._restaurants_service.listRestaurantComment(this.restaurante.id).subscribe(comentarios => {
+      this.array_comentarios_usuarios = comentarios.map(comentario => comentario);
+      comentarios.forEach(comentario => {
+        return this.media_geral.push(comentario["estrelas"]);
       });
-    this.array_comentarios_usuarios.forEach((comentario: any) => {
-      this.media_geral.push(comentario["estrelas"]);
+      this.media_geral.push(param);
+      const sum = this.media_geral.reduce((a: number, b: number) => a + b, 0);
+      const toDisplay = Math.round(sum / this.media_geral.length);
+      return this.avaliacao = toDisplay;
     });
-    this.media_geral.push(param);
-    const sum = this.media_geral.reduce((a: number, b: number) => a + b, 0);
-    const toDisplay = Math.round(sum / this.media_geral.length);
-    return this.avaliacao = toDisplay;
   }
 
   formatDate(seconds: number) {
